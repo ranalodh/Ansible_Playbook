58e23fe432489a12527c46079980b24e7972ca1e
Ranadip Lodh <ranalodh@in.ibm.com>
2019-11-26 15:40:54 +0530

    update

--- backup/indosat/monitordisk.sh
+++ backup/indosat/monitordisk.sh
@@ -14,7 +14,6 @@ do
   usep=$(echo $output | awk '{ print $1}' | cut -d'%' -f1  )
   partition=$(echo $output | awk '{ print $2 }' )
   if [ $usep -ge $ALERT ]; then
-    echo "Running out of space \"$partition ($usep%)\" on $(hostname) as on $(date)" #| 
-   # mail -s "Alert: Almost out of disk space $usep" $ADMIN
+    echo "Running out of space \"$partition ($usep%)\" on $(hostname) as on $(date)"
   fi
 done
